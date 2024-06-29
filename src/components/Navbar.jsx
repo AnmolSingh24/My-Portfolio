92b862a296e7cbcf83fd20ef0279e460c157a4e2
Anmol Singh <as0796348@gmail.com>
2024-06-29 18:58:15 +0530

    Initial commit

---
+++ src/components/Navbar.jsx
@@ -0,0 +1,19 @@
+import { FaLinkedin } from "react-icons/fa";
+import { FaInstagram } from "react-icons/fa";
+
+const Navbar = () => {
+  return (
+    <nav className="mb-20 flex items-center justify-between py-6">
+      <div className="flex flex-shrink-0 items-center">
+        <img className="mx-2 w-10" src="#" alt="logo" />
+      </div>
+
+      <div className="m-8 flex justify-end items-center gap-4 text-2xl">
+        <FaLinkedin />
+        <FaInstagram />
+      </div>
+    </nav>
+  )
+}
+
+export default Navbar
