02a0e038e9a7f5f2a45744dab4139dee8ce4cab3
Anmol Singh <as0796348@gmail.com>
2024-07-01 14:03:28 +0530

    Contact section completed

--- src/components/ContactUs.jsx
+++ src/components/ContactUs.jsx
@@ -1,17 +1,30 @@
 import React from 'react'
 import { CONTACT } from '../constants'
+import { motion } from 'framer-motion'
 
 const ContactUs = () => {
   return (
     <div className="border-b border-neutral-900 pb-4">
-      <h2 className="my-10 text-center text-4xl">Get In Touch</h2>
+      <motion.h2
+        whileInView={{ opacity: 1, y: 0 }}
+        initial={{ opacity: 0, y: -100 }}
+        transition={{ duration: 0.5 }}
+        className="my-10 text-center text-4xl">Get In Touch</motion.h2>
       <div className="text-center tracking-tighter">
-        <p className="my-4">Contact:
+        <motion.p
+          whileInView={{ opacity: 1, x: 0 }}
+          initial={{ opacity: 0, x: 100 }}
+          transition={{ duration: 1 }}
+          className="my-4">Contact:
           <span> {CONTACT.phoneNo}</span>
-        </p>
-        <p className="my-4">Email:
+        </motion.p>
+        <motion.p
+          whileInView={{ opacity: 1, x: 0 }}
+          initial={{ opacity: 0, x: -100 }}
+          transition={{ duration: 1 }}
+          className="my-4">Email:
           <span className="border-b"> {CONTACT.email}</span>
-        </p>
+        </motion.p>
       </div>
     </div>
   )
